45bd73f72bf999f8c06c894770c20f92e35c82d7
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-01-09 17:23:54 +0100

    Improve DesktopConsole.lua

--- files/DesktopConsole.lua
+++ files/DesktopConsole.lua
@@ -1,72 +1,11 @@
-if get_window_name() == "DesktopConsole1" then
-   set_window_workspace(1)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole2" then
-   set_window_workspace(2)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole3" then
-   set_window_workspace(3)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole4" then
-   set_window_workspace(4)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole5" then
-   set_window_workspace(5)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole6" then
-   set_window_workspace(6)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole7" then
-   set_window_workspace(7)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
-end
-if get_window_name() == "DesktopConsole8" then
-   set_window_workspace(8)
-   set_window_below()
-   undecorate_window()
-   set_skip_pager(true)
-   set_skip_tasklist(true)
-   set_window_type("WINDOW_TYPE_UTILITY")
-   set_window_geometry2(50,50,1180,680)
+for i=1,8 do
+    if get_window_name() == "DesktopConsole" .. i then
+        set_window_workspace(i)
+        set_window_below()
+        undecorate_window()
+        set_skip_pager(true)
+        set_skip_tasklist(true)
+        set_window_type("WINDOW_TYPE_UTILITY")
+        set_window_geometry2(50,50,1180,680)
+    end
 end
