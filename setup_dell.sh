548679ec0fc24c9695bea9ac45d7db106fe72d67
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-01-10 15:58:29 +0100

    Extra roles added to all platforms.

--- setup_dell.sh
+++ setup_dell.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-ANSIBLE_CONFIG=ansible_work.cfg ansible-playbook site.yml -i hosts -e @vars_work.yml --ask-sudo-pass
+#FIXME EXTRA_ROLE_PATH duplicates ansible_extra.cfg/roles_path
+ANSIBLE_CONFIG=ansible_extra.cfg EXTRA_ROLE_PATH="$(pwd)/../0050_ansible_extra" ansible-playbook site.yml -i hosts -e @vars_work.yml --ask-sudo-pass
--- setup_hp.sh
+++ setup_hp.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-ansible-playbook site.yml -i hosts -e @vars_home.yml --ask-sudo-pass --tags legacy
+#FIXME EXTRA_ROLE_PATH duplicates ansible_extra.cfg/roles_path
+ANSIBLE_CONFIG=ansible_extra.cfg EXTRA_ROLE_PATH="$(pwd)/../0050_ansible_extra" ansible-playbook site.yml -i hosts -e @vars_home.yml --ask-sudo-pass --tags legacy
