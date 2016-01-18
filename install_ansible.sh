61cf0ea8e784e6e370a27e9fda81c37cb6fe04e8
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-01-18 22:52:36 +0100

    Fix ansible installation on crouton.

--- install_ansible.sh
+++ install_ansible.sh
@@ -9,5 +9,5 @@ apt-get update
 
 # Don't use 2.0.0.1 because we hit https://github.com/ansible/ansible/issues/13763
 #apt-get install ansible
-apt-get install python-support python-jinja2 python-yaml python-paramiko sshpass
+apt-get install python-support python-jinja2 python-yaml python-paramiko python-httplib2 sshpass
 dpkg -i files/ansible_1.9.4-1ppa~precise_all.deb
