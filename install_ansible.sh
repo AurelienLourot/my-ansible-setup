d806fdfcc4a2ef2eb2a782ff30a539a60f827d15
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-01-18 21:25:12 +0100

    Fix ansible installation on Ubuntu 14.04.

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
+set -xe
+
+apt-get update
 apt-get install software-properties-common python-software-properties
 apt-add-repository ppa:ansible/ansible
 apt-get update
 
 # Don't use 2.0.0.1 because we hit https://github.com/ansible/ansible/issues/13763
 #apt-get install ansible
+apt-get install python-support python-jinja2 python-yaml python-paramiko sshpass
 dpkg -i files/ansible_1.9.4-1ppa~precise_all.deb
