55c9b0402ceb83e9dceddb8655389a04f4a24ac5
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-01-29 23:19:26 +0100

    gitupdate improved.

--- files/gitupdate.sh
+++ files/gitupdate.sh
@@ -1,8 +1,29 @@
+#!/usr/bin/env bash
+
 ROOT_DIR=~/Documents/git/
 
+failed_repos=""
+list_branches_behind=""
 for i in $(find -L $ROOT_DIR -name .git)
 do
     cd $(dirname $i)
     pwd
     git rpupdate
+    result=$?
+    if [[ result -ne "0" ]]
+    then
+        failed_repos="${failed_repos}$(pwd)\n"
+    fi
+
+    branches_behind=`git branch -v | grep behind`
+    if [[ ! -z "$branches_behind" ]]
+    then
+        list_branches_behind="${list_branches_behind}$(pwd)\n${branches_behind}\n"
+    fi
 done
+
+echo
+echo "Failed to fetch from:"
+echo -e "$failed_repos"
+echo "Branches behind:"
+echo -e "$list_branches_behind"
