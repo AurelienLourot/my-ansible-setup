def105e19cc37d19aeb4bff6ac94514d3b34fbeb
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-02-18 18:37:49 +0100

    xdotool fixed.

--- files/xbindkeysrc.scm
+++ files/xbindkeysrc.scm
@@ -1,3 +1,4 @@
-; Map Alt+click to middle button
+; Map Shift+click to middle button
 ; Map on Release so that it does not appear both buttons are pressed
-(xbindkey '(release alt "b:1") "xdotool click --clearmodifiers 2")
+; See http://lourot.com/articles/crouton-emacs-middle-click
+(xbindkey '(release shift "b:1") "xdotool click --clearmodifiers 2")
