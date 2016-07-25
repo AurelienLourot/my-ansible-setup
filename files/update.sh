ec5d3eb392d45ff45bfcf225082f2cd19474b7c2
Aurelien Lourot <aurelien.lourot@gmail.com>
2016-07-25 21:15:38 +0200

    myberl.in added to update script.

--- files/update.sh
+++ files/update.sh
@@ -7,6 +7,9 @@ ROOT_DIR=~/Documents/git/
 cd $ROOT_DIR/google-drive/grive/
 grive
 
+cd $ROOT_DIR/myberl.in/snapshots/maps/thirdparty/
+./download.sh
+
 cd $ROOT_DIR/cliist/
 ./cliist.py -A > $ROOT_DIR/todoist/todoist.txt
 
