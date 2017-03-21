937340b135f146a5fc8f96cec0dac7fddf86a7e7
Aurelien Lourot <aurelien.lourot@gmail.com>
2017-03-21 23:39:04 +0100

    Replace cliist by todoist-cli.

--- files/update.sh
+++ files/update.sh
@@ -10,8 +10,11 @@ grive
 cd $ROOT_DIR/myberl.in/snapshots/maps/thirdparty/
 ./download.sh
 
-cd $ROOT_DIR/cliist/
-./cliist.py -A > $ROOT_DIR/todoist/todoist.txt
+cd $ROOT_DIR/todoist/
+todoist s
+todoist l > todoist.txt
+git commit --allow-empty -am .
+git push
 
 SCRIPT_DIR=$(dirname $0)
 $SCRIPT_DIR/gitupdate.sh
