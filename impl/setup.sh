85a857bef31890a3a57aaeaf4f17094a2c7a6210
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-03-20 22:48:53 +0200

    Removed support for extra roles.

--- impl/setup.sh
+++ impl/setup.sh
@@ -7,5 +7,4 @@
 
 tail -F --pid=$$ --lines=0 ansible_apt.log 2> /dev/null &
 
-#FIXME EXTRA_ROLE_PATH duplicates ansible_extra.cfg/roles_path
-ANSIBLE_CONFIG=ansible_extra.cfg EXTRA_ROLE_PATH="$(pwd)/../0050_ansible_extra" ansible-playbook site.yml -i hosts --ask-sudo-pass --timeout=10 "$@"
+ansible-playbook site.yml -i hosts --ask-sudo-pass --timeout=10 "$@"
