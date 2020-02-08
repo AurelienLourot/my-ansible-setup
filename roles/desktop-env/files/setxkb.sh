b36b3a2dec4d78fa78ebe378b8806b4edfdb8cdf
Aurelien Lourot <aurelien.lourot@gmail.com>
2020-02-08 13:17:34 +0100

    Keyboard mapping.

--- roles/desktop-env/files/setxkb.sh
+++ roles/desktop-env/files/setxkb.sh
@@ -9,3 +9,12 @@ xmodmap -e "keycode 66 = F13"
 xmodmap -e "keycode 133 = F13"
 
 xmodmap -e "keycode 22 shift = BackSpace Delete"
+
+## Useful stuff for when you break your left wrist ;)
+##
+## # https://askubuntu.com/questions/375056/re-mapping-alt-gr-key
+## xmodmap -e "clear mod5"; sleep 1
+## xmodmap -e "keycode 108 = Alt_L"
+##
+## xmodmap -e "keycode 21 = at"
+## xmodmap -e "keycode 94 mod1 = pipe"
