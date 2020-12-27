1447be4211d982171b19d1bfb779fb48f62cdb8c
Aurelien Lourot <aurelien.lourot@gmail.com>
2020-12-27 15:39:27 +0100

    Unpinned ansible.

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@
 
 tail -F --pid=$$ --lines=0 ansible_apt.log 2> /dev/null &
 
-ansible-playbook site.yml -i hosts --ask-sudo-pass --timeout=10 "$@"
+ansible-playbook site.yml -i hosts --ask-become-pass --timeout=10 "$@"
